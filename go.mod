6f399bf71d4b1067b969956820db50310df48772
Ian <ian.thomas@tiani-spirit.com>
2022-09-12 20:02:41 +0100

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ipthomas/tukutil
 
 go 1.19
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/google/uuid v1.3.0
+	github.com/ipthomas/tukcnst v1.0.3
+)
--- tukutil.go
+++ tukutil.go
@@ -2,39 +2,54 @@ package tukutil
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/fs"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
 	"strings"
+	"syscall"
 	"text/template"
 	"time"
+
+	cnst "github.com/ipthomas/tukcnst"
 
 	"github.com/google/uuid"
 )
 
 var (
-	SeedRoot   = "1.2.40.0.13.1.1.3542466645."
-	IdSeed     = getIdIncrementSeed(5)
-	CodeSystem = make(map[string]string)
+	ServerName, _ = os.Hostname()
+	SeedRoot      = "1.2.40.0.13.1.1.3542466645."
+	IdSeed        = getIdIncrementSeed(5)
+	CodeSystem    = make(map[string]string)
 )
 
 // TemplateFuncMap returns a functionMap of tukutils for use in templates
 func TemplateFuncMap() template.FuncMap {
 	return template.FuncMap{
-		"dtday":      Tuk_Day,
-		"dtmonth":    Tuk_Month,
-		"dtyear":     Tuk_Year,
-		"mappedid":   GetCodeSystemVal,
-		"prettytime": PrettyTime,
-		"newuuid":    NewUuid,
-		"newid":      Newid,
-		"tuktime":    Tuk_Time,
+		"dtday":         Tuk_Day,
+		"dtmonth":       Tuk_Month,
+		"dtyear":        Tuk_Year,
+		"mapval":        GetCodeSystemVal,
+		"prettytime":    PrettyTime,
+		"newuuid":       NewUuid,
+		"newid":         Newid,
+		"tuktime":       Time_Now,
+		"isafternow":    IsAfterNow,
+		"duration":      GetDuration,
+		"durationsince": GetDurationSince,
+		"hasexpired":    OHT_HasExpired,
+		"completedate":  OHT_CompleteByDate,
 	}
 }
+func SetResponseHeaderServer(tukServerName string) {
+	ServerName = tukServerName
+}
 
-// SplitXDWKey takes a string input (xdw key) and returns the pathway and nhs id for the xdw
+// SplitXDWKey takes a string input (xdwkey) and returns the pathway and nhs id for the xdw
 func SplitXDWKey(xdwkey string) (string, string) {
 	var pwy string
 	var nhs string
@@ -46,13 +61,37 @@ func SplitXDWKey(xdwkey string) (string, string) {
 	return pwy, nhs
 }
 
+func WriteResponseHeaders(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Server", ServerName)
+		if r.Header.Get(cnst.ACCEPT) == cnst.APPLICATION_JSON {
+			w.Header().Set(cnst.CONTENT_TYPE, cnst.APPLICATION_JSON)
+		} else {
+			if r.Header.Get(cnst.ACCEPT) == cnst.APPLICATION_XML {
+				w.Header().Set(cnst.CONTENT_TYPE, cnst.APPLICATION_XML)
+			} else {
+				w.Header().Set(cnst.CONTENT_TYPE, cnst.TEXT_HTML)
+			}
+
+		}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "accept, Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+
+		fn(w, r)
+	}
+}
+
 // SetCodeSystem takes a map input and sets the codesystem map with the input
 func SetCodeSystem(cs map[string]string) {
 	CodeSystem = cs
+	log.Printf("Loaded %v code system key values", len(CodeSystem))
+	Log(CodeSystem)
 }
 
-// InitCodeSystem loads the codesystem json file and sets the codesystem map from the json file values
-func InitCodeSystem(codesystemFile string) error {
+// LoadCodeSystemFile loads the `codesystem.json` file from the `configs` folder
+func LoadCodeSystemFile(codesystemFile string) error {
 	file, err := os.Open(codesystemFile)
 	if err != nil {
 		log.Println(err.Error())
@@ -66,18 +105,68 @@ func InitCodeSystem(codesystemFile string) error {
 	Log(CodeSystem)
 	return nil
 }
-func Log(i interface{}) {
-	b, _ := json.MarshalIndent(i, "", "  ")
-	log.Println(string(b))
-}
 
-// GetCodeSystemVal takes a string input (key) and returns the string value corresponding to the input (key) from the codesystem
+// GetCodeSystemVal takes a string input (key) and returns from the codesystem the string value corresponding to the input (key)
 func GetCodeSystemVal(key string) string {
 	val, ok := CodeSystem[key]
 	if ok {
 		return val
 	}
 	return key
+}
+
+// CreateLog checks if the log folder exists and creates it if not. It then checks for a subfolder for the current year i.e. 2022 and creates it if it does not exist. It then checks for a log file with a name equal to the current day and month and extension .log i.e. 0905.log. If it exists log output is appended to the existing file otherwise a new log file is created.
+func CreateLog(log_Folder string) *os.File {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+	mdir := log_Folder
+	if _, err := os.Stat(mdir); errors.Is(err, fs.ErrNotExist) {
+		if e2 := os.Mkdir(mdir, 0700); e2 != nil {
+			log.Println(err.Error())
+			return nil
+		}
+	}
+	dir := mdir + "/" + Tuk_Year()
+	if _, err := os.Stat(dir); errors.Is(err, fs.ErrNotExist) {
+		if e2 := os.Mkdir(dir, 0700); e2 != nil {
+			log.Println(err.Error())
+			return nil
+		}
+	}
+	logFile, err := os.OpenFile(dir+"/"+Tuk_Month()+Tuk_Day()+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	log.Println("Using log file - " + logFile.Name())
+	log.SetOutput(logFile)
+	log.Println("-----------------------------------------------------------------------------------")
+	return logFile
+}
+func MonitorApp() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		signalType := <-ch
+		signal.Stop(ch)
+		fmt.Println("")
+		fmt.Println("***")
+		fmt.Println("Exit command received. Exiting...")
+		exitcode := 0
+		switch signalType {
+		case os.Interrupt:
+			log.Println("FATAL: CTRL+C pressed")
+		case syscall.SIGTERM:
+			log.Println("FATAL: SIGTERM detected")
+			exitcode = 1
+		}
+		os.Exit(exitcode)
+	}()
+}
+
+// Log takes any struc as input and logs out the struc as a json string
+func Log(i interface{}) {
+	b, _ := json.MarshalIndent(i, "", "  ")
+	log.Println(string(b))
 }
 
 // returns unique id in format '1.2.40.0.13.1.1.3542466645.20211021090059143.32643'
@@ -91,14 +180,121 @@ func Newid() string {
 	IdSeed = IdSeed + 1
 	return id
 }
-
-// Tuk_Pretty_Time returns a pretty version of the current time for location Europe/London (strips everything after the `.` in Tuk_Time)
-func TUK_Pretty_Time() string {
-	return PrettyTime(Tuk_Time())
+func OHT_HasExpired(startdate string, htDate string) bool {
+	log.Printf("Calculating if Expired")
+	if expireDate := OHT_CompleteByDate(startdate, htDate); expireDate == "NA" {
+		return false
+	} else {
+		if exdate, err := time.Parse("2006-01-02 15:04:05", expireDate); err != nil {
+			log.Println(err.Error())
+			return false
+		} else {
+			log.Printf("Has Expired = %v", time.Now().After(exdate))
+			return time.Now().After(exdate)
+		}
+	}
 }
 
-// Tuk_Time returns the current time for location Europe/London.
-func Tuk_Time() string {
+// OHT_CompletionByDate takes a 'start date' input as a rfc3339 formatted string and the number of days in the future as a string containing an OASIS Human Task xdw function `day(x)“ where x is number of days in the future from the `start date`
+// It returns a string containing the future date. If there is an error it returns `NA`
+//
+//	For example - OHT_CompleteByDate("2022-09-06T23:06:54+01:00", "day(10)") returns `2022-09-16 23:06:54`
+func OHT_CompleteByDate(startdate string, htDate string) string {
+	var err error
+	var futuredays int
+	var st time.Time
+	var ft time.Time
+	if strings.Contains(htDate, "(") && strings.Contains(htDate, ")") {
+		if futuredays, err = strconv.Atoi(strings.Split(strings.Split(htDate, "(")[1], ")")[0]); err == nil {
+			log.Printf("Calculating date %v Days from %s", futuredays, startdate)
+			if st, err = time.Parse(time.RFC3339, startdate); err == nil {
+				ft = st.Add(time.Hour * 24 * time.Duration(futuredays))
+				log.Printf("Complete By %s", PrettyTime(ft.String()))
+				return PrettyTime(ft.String())
+			}
+		}
+	}
+	log.Println(err.Error())
+	return "NA"
+}
+
+// GetDurationSince takes a time as string input in RFC3339 format (yyyy-MM-ddThh:mm:ssZ) and returns the duration in days, hours and mins in a 'pretty format' eg '2 Days 0 Hrs 52 Mins' between the provided time and time.Now() as a string
+func GetDurationSince(stime string) string {
+	log.Println("Obtaining time Duration since - " + stime)
+	st, err := time.Parse(time.RFC3339, stime)
+	if err != nil {
+		log.Println(err.Error())
+		return "Not Available"
+	}
+	dur := time.Since(st)
+	log.Printf("Duration - %v", dur.String())
+	days := 0
+	hrs := int(dur.Hours())
+	min := int(dur.Minutes())
+
+	if hrs > 24 {
+		days = hrs / 24
+		hrs = hrs % 24
+	}
+	daysstr := strconv.Itoa(days)
+	hrsstr := strconv.Itoa(hrs)
+	minstr := strconv.Itoa(min - (days * 24 * 60) - (hrs * 60))
+	log.Println("Returning " + daysstr + " Days " + hrsstr + " Hrs " + minstr + " Mins")
+	return daysstr + " Days " + hrsstr + " Hrs " + minstr + " Mins"
+}
+
+// GetDuration takes 2 times as string inputs in RFC3339 format (yyyy-MM-ddThh:mm:ssZ) and returns the duration in days, hours and mins in a 'pretty format' eg '2 Days 0 Hrs 52 Mins' between the provided times as a string
+//
+//	Example : GetDuration("2022-09-04T13:15:20Z", "2022-09-14T16:20:01Z") returns `10 Days 3 Hrs 4 Mins`
+func GetDuration(stime string, etime string) string {
+	log.Println("Obtaining time Duration between - " + stime + " and " + etime)
+	st, err := time.Parse(time.RFC3339, stime)
+	if err != nil {
+		log.Println(err.Error())
+		return "Not Available"
+	}
+	et, err := time.Parse(time.RFC3339, etime)
+	if err != nil {
+		log.Println(err.Error())
+		return "Not Available"
+	}
+	dur := et.Sub(st)
+	days := 0
+	hrs := int(dur.Hours())
+	min := int(dur.Minutes())
+	if hrs > 24 {
+		days = hrs / 24
+		hrs = hrs % 24
+	}
+	daysstr := strconv.Itoa(days)
+	hrsstr := strconv.Itoa(hrs)
+	minstr := strconv.Itoa(min - (days * 24 * 60) - (hrs * 60))
+	log.Println("Returning " + daysstr + " Days " + hrsstr + " Hrs " + minstr + " Mins")
+	return daysstr + " Days " + hrsstr + " Hrs " + minstr + " Mins"
+}
+
+// IsAfterNow takes a time as a string input in RFC3339 format (yyyy-MM-ddThh:mm:ssZ) and returns true if the input time is after time.Now() and false if input time is before time.Now()
+func IsAfterNow(inTime string) bool {
+	log.Printf("Checking if %s is after the current time", inTime)
+	it, err := time.Parse(time.RFC3339, inTime)
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+	now := time.Now().Local()
+	log.Println("Time Now - " + now.Local().String())
+	log.Println("Start Time - " + it.Local().String())
+	log.Printf("Time %s IsAfter(time.Now()) = %v", inTime, now.Before(it))
+	return now.Before(it)
+}
+
+// Pretty_Time_Now returns a pretty version of the current time for location Europe/London (strips everything after the `.` in Tuk_Time)
+func Pretty_Time_Now() string {
+	return PrettyTime(Time_Now())
+}
+
+// Time_Now returns the current time for location Europe/London.
+func Time_Now() string {
 	location, err := time.LoadLocation("Europe/London")
 	if err != nil {
 		log.Println(err.Error())
@@ -107,9 +303,9 @@ func Tuk_Time() string {
 	return time.Now().In(location).String()
 }
 
-// PrettyTime strips everything after the `.` from the input time string
+// PrettyTime fist splits the input based on sep =`.`, it takes index 0 of the split and replaces any T with a space then removes any trailing Z. It then splits the resulting string on sep = `+` returning index 0 of the split
 func PrettyTime(time string) string {
-	return strings.TrimSuffix(strings.ReplaceAll(strings.Split(time, ".")[0], "T", " "), "Z")
+	return strings.TrimSuffix(strings.Split(strings.TrimSuffix(strings.ReplaceAll(strings.Split(time, ".")[0], "T", " "), "Z"), "+")[0], " ")
 }
 
 // TUK_Hour returns the current hour as a 2 digit string
